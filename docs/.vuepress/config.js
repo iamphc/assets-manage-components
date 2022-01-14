38a45292cc8113a6188724e4750974a5ce499011
iamphc <iamthephc@gmail.com>
2022-01-14 14:51:14 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "门户-资产管理-前端开发文档",
+  description: "",
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '通用组件', link: '/common-components/' },
+      { text: '业务组件', link: '/business-components/' },
+      { text: '页面组件', link: '/page-components/' },
+      { text: '工具函数', link: '/function-tools/' },
+      { text: '枚举常量', link: '/enum-constant/' }
+    ]
+  }
+}
